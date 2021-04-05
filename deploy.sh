3d40d687eeb71cb898b3a625829376743afa344d
Mayank Rastogi <45353658+rastogi-mayank@users.noreply.github.com>
2021-04-05 12:38:11 +0530

    removed env variable

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 echo $(git rev-parse HEAD)
-docker build -t mayankrastogi94/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t mayankrastogi94/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t mayankrastogi94/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+echo $SHA
+docker build -t mayankrastogi94/multi-client:$(git rev-parse HEAD) -f ./client/Dockerfile ./client
+docker build -t mayankrastogi94/multi-server:$(git rev-parse HEAD) -f ./server/Dockerfile ./server
+docker build -t mayankrastogi94/multi-worker:$(git rev-parse HEAD) -f ./worker/Dockerfile ./worker
 
-docker push mayankrastogi94/multi-client:$SHA
-docker push mayankrastogi94/multi-server:$SHA
-docker push mayankrastogi94/multi-worker:$SHA
+docker push mayankrastogi94/multi-client:$(git rev-parse HEAD)
+docker push mayankrastogi94/multi-server:$(git rev-parse HEAD)
+docker push mayankrastogi94/multi-worker:$(git rev-parse HEAD)
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=mayankrastogi94/multi-server:$SHA
-kubectl set image deployments/client-deployment client=mayankrastogi94/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=mayankrastogi94/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=mayankrastogi94/multi-server:$(git rev-parse HEAD)
+kubectl set image deployments/client-deployment client=mayankrastogi94/multi-client:$(git rev-parse HEAD)
+kubectl set image deployments/worker-deployment worker=mayankrastogi94/multi-worker:$(git rev-parse HEAD)
